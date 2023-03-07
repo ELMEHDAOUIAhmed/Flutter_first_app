49b815dbb49689517f02de9e18116c5930f47dae
ELMEHDAOUIAhmed <elmehdaoui.ahmed77@gmail.com>
2023-03-07 18:34:52 +0100

    updated app

--- TIPS.md
+++ TIPS.md
@@ -1,6 +1,22 @@
-every widget in flutter needs to extend "Stateless widget or stateful widget"
+#very widget in flutter needs to extend "Stateless widget or stateful widget"
 
-force you to add build method to draw pixels into the screen  
+#force you to add build method to draw pixels into the screen  
 
-@override 
+#@override 
 " decorative "
+
+#anonymous function :
+onPressed: ()=> print('Answer 2! chosen'),
+
+or
+
+onPressed: (){ print('Answer 3! chosen');},
+
+you cant call it from any where else because it deosnt have a name
+
+
+
+#imagine Widget build as Loop() in arduino
+it reexecutes each time flutter want to rebuild a screen
+so you ATTENTION dont define dynamique variables there it will cause issues 
+it will be redefined each time it gets rebuild
--- myfirst_app/lib/main.dart
+++ myfirst_app/lib/main.dart
@@ -2,15 +2,45 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyApp());
-  
-  
 }
 
-class MyApp extends StatelessWidget{
-  Widget build(BuildContext context){
-    return MaterialApp(home: Text('Hello'),);
+//void main => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+
+void answerQuestion()
+{
+  print('Answer 1! chosen');
+}
+
+  Widget build(BuildContext context) {
+    var qst = [
+      'whats \'s your favorite color ? :',
+      'Whats \'s your favorite animal ? ',
+    ];
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('hi'),
+        ),
+        body: Column(
+          children: [
+            Text(qst[1]) ,
+            ElevatedButton(
+              onPressed: answerQuestion,
+              child: Text('Answer 1'),
+            ),
+            ElevatedButton(
+              onPressed: ()=> print('Answer 2! chosen'),
+              child: Text('Answer 2'),
+            ),
+            ElevatedButton(
+              onPressed: (){ print('Answer 3! chosen');},
+              child: Text('Answer 3'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
-
-
-
 }
